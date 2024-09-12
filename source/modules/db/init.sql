d3162a2f01a3d9ad74862c73288a33448e5de1a8
Nicholasgboland <59149236+Nicholasgboland@users.noreply.github.com>
2024-09-12 09:15:31 -0600

    Update init.sql

--- source/modules/db/init.sql
+++ source/modules/db/init.sql
@@ -20,11 +20,40 @@ CREATE TABLE property (
 
 CREATE TABLE unit (
   unit_id SERIAL NOT NULL,
+  property_id INTEGER NOT NULL,
   name VARCHAR(100) NOT NULL,
   unit_number VARCHAR(50) NOT NULL,
   sqr_ft NUMERIC(10,5),
   price_per_sqqr_ft NUMERIC(50,2),
   notes VARCHAR(200),
   PRIMARY KEY(unit_id),
-  CONSTRAINT unit_property_fk FOREIGN KEY(unit_id) REFERENCES property(property_id)
+  CONSTRAINT unit_property_fk FOREIGN KEY(property_id) REFERENCES property(property_id)
+);
+
+CREATE TABLE tenant (
+  tenant_id SERIAL NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  phone VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  notes VARCHAR(200)
+);
+
+CREATE TABLE rental_agreement (
+  agreement_id SERIAL NOT NULL,
+  property_id INTEGER NOT NULL,
+  tenant_id INTEGER NOT NULL,
+  contract_id VARCHAR(50),
+  contract_docs TEXT [],
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  curr_rent NUMERIC(50,2) NOT NULL,
+  accepted BOOLEAN NOT NULL,
+  active BOOLEAN NOT NULL,
+  payment_sched VARCHAR(50),
+  notes VARCHAR(200),
+  next_increase DATE,
+  last_increase DATE,
+  PRIMARY KEY(agreement_id),
+  CONSTRAINT unit_property_fk FOREIGN KEY(property_id) REFERENCES property(property_id),
+  CONSTRAINT unit_ten_fk FOREIGN KEY(tenant_id) REFERENCES tenant(tenant_id)
 );
