30c1f04c1b5fc9fe7b3f046b8424a2696b15316d
Nicholasgboland <59149236+Nicholasgboland@users.noreply.github.com>
2024-10-01 10:36:46 -0600

    Create db_interface.py

---
+++ source/modules/db/db_interface.py
@@ -0,0 +1,20 @@
+import psycopg2
+from psycopg2 import connect, Error
+import contextlib
+from getpass import getpass
+import json
+from string import Formatter
+
+def initQueries:
+  with opne("SQL.json") as query_file:
+    query_dict = json.load(query_file)
+  return query_dict
+
+def getQuery(queryName, query_dict):
+  query = query_dict[name]
+  parameters = [pname for _, pname, _, _ in Formatter().parse(query) if pname]
+  parameters_dict = []
+  for param in parameters:
+    parameters_dict[i] = "NULL"
+  return query, parameters_dict
+
