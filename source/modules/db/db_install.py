a56837ba1911d13d592f59cfc18920f7befacc3b
Nicholasgboland <59149236+Nicholasgboland@users.noreply.github.com>
2024-10-02 10:08:57 -0600

    Update db_install.py

--- source/modules/db/db_install.py
+++ source/modules/db/db_install.py
@@ -9,3 +9,19 @@ DBinfo["port"] = input("Enter install database port (default: ###): ")
 DBinfo["host"] = input("Enter install database port (default: ###): ")
 DBinfo["user"] = input("Enter install database username (default: postgres): ")
 DBinfo["passwd"] = getpass("Enter install database password: ")
+
+with open('init-db_real_estate.sql') as infile:
+  for row in infile:
+    try:
+      with contextlib.closing(connect(
+        host=DBinfo["host"],
+        user=DBinfo["user"],
+        password=DBinfo["passwd"],
+        port=DBinfo["port"],
+        database=DBinfo["DB"],
+      )) as connection:
+        connection.autocommit = True
+          with connection.cursor() as cursor:
+            cursor.execute(execSQL)
+    except Error as e:
+      print("Error: ", e)
