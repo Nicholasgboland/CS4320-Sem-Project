36ab18d8570581fa74946accfcc2efdcfe103e6a
Nicholasgboland <59149236+Nicholasgboland@users.noreply.github.com>
2024-10-03 08:35:53 -0600

    Update db_install.py

--- source/modules/db/db_install.py
+++ source/modules/db/db_install.py
@@ -4,7 +4,7 @@ from psycopg2 import connect, Error
 from getpass import getpass
 
 DBinfo ={}
-DBinfo["DB"] = input("Enter install database name (default: postgres): ")
+DBinfo["DB"] = "postgres"
 DBinfo["port"] = input("Enter install database port (default: ###): ")
 DBinfo["host"] = input("Enter install database port (default: ###): ")
 DBinfo["user"] = input("Enter install database username (default: postgres): ")
