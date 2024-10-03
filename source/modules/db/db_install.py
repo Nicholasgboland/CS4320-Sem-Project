3f1535997914424458abb30c67e99fd33ff2dbbd
Nicholasgboland <59149236+Nicholasgboland@users.noreply.github.com>
2024-10-03 09:11:28 -0600

    Update db_install.py

--- source/modules/db/db_install.py
+++ source/modules/db/db_install.py
@@ -2,16 +2,52 @@
 import psycopg2
 from psycopg2 import connect, Error
 from getpass import getpass
+from string import Formatter
 
 DBinfo ={}
-DBinfo["DB"] = "postgres"
-DBinfo["port"] = input("Enter install database port (default: ###): ")
-DBinfo["host"] = input("Enter install database port (default: ###): ")
-DBinfo["user"] = input("Enter install database username (default: postgres): ")
-DBinfo["passwd"] = getpass("Enter install database password: ")
+adminDBinfo["DB"] = "postgres"
+adminDBinfo["port"] = input("Enter install database port (default: ###): ")
+adminDBinfo["host"] = input("Enter install database port (default: ###): ")
+adminDBinfo["user"] = input("Enter install database username (default: postgres): ")
+adminDBinfo["passwd"] = getpass("Enter install database password: ")
+
+print("Creating new login user, this will be the username/password that will be used to login to the application:")
+DBinfo["DB"] = adminDBinfo["DB"]
+DBinfo["port"] = adminDBinfo["port"]
+DBinfo["host"] = adminDBinfo["host"]
+DBinfo["user"] = input("Enter new database username: ")
+DBinfo["passwd"] = getpass("Enter new database password: ")
+
+createUsrSQL = "CREATE USER {newUser} WITH PASSWORD '{newPasswd}'".format(newUser = DBinfo["user"], newPasswd = DBinfo["passwd"])
+grant1SQL = "GRANT CONNECT ON DATABASE real_estate TO {newUser}".format(newUser = DBinfo["user"])
+grant2SQL = "GRANT ALL PRIVILEGES ON DATABASE real_estate to {newUser}".format(newUser = DBinfo["user"])
+
+
+try:
+    with contextlib.closing(connect(
+      host=adminDBinfo["host"],
+      user=adminDBinfo["user"],
+      password=adminDBinfo["passwd"],
+      port=adminDBinfo["port"],
+      database=adminDBinfo["DB"],
+    )) as connection:
+      connection.autocommit = True
+        with connection.cursor() as cursor:
+          directory = []
+          cursor.execute("SHOW data_directory")
+          if cursor.definition is not None:
+            directory = cursor.fetchall()
+            DBinfo["directory"] = directory
+          cursor.execute("CREATE DATABASE real_estate")
+          cursor.execut(createUsrSQL)
+          cursor.execut(grant1SQL)
+          cursor.execut(grant2SQL)
+  except Error as e:
+    print("Error: ", e)
 
 with open('init-db_real_estate.sql') as infile:
-  for row in infile:
+  lines = infile.readlines()
+  for line in lines:
     try:
       with contextlib.closing(connect(
         host=DBinfo["host"],
@@ -25,3 +61,7 @@ with open('init-db_real_estate.sql') as infile:
             cursor.execute(execSQL)
     except Error as e:
       print("Error: ", e)
+
+with open('DB_config.json', 'w') as outfile:
+  jsonFile = json.dumps(DBinfo, indent=4)
+  outfile.write(jsonFile)
