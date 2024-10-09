02be4b8434e145c02024a30ad3443386a97b2d16
Nicholasgboland <59149236+Nicholasgboland@users.noreply.github.com>
2024-10-09 15:44:34 -0600

    Update simple_real_estate.py

--- source/simple_real_estate.py
+++ source/simple_real_estate.py
@@ -1,10 +1,16 @@
 import modules
 from modules import db
+# from modules import db, gui
 DB = db.db_interface
 
 def login():
-  # Call to GUI code here
-    DBInfo = initDBInfo(username, password)
+    # Call to GUI login code here (returns username, password
+    DBInfo = DB.initDBInfo(username, password)
     testCon = DB.testConnection(DBInfo)
     if testCon != 0:
-        return 1
+        return None
+    queries = DB.initQueries()
+    return DBInfo, queries
+
+def launch_management_screen(DBInfo):
+    # Code to launch the initial GUI screen
