c47e3d30e9da41cdff9326d0c6966f4aeac2789a
Nicholasgboland <59149236+Nicholasgboland@users.noreply.github.com>
2024-10-21 14:48:08 -0600

    Update simple_real_estate.py

--- source/simple_real_estate.py
+++ source/simple_real_estate.py
@@ -1,13 +1,23 @@
-import modules
+import modules, sys
 from modules import db
 # from modules import db, gui
 DB = db.db_interface
 
 def setup():
-    # Call to GUI login code here (returns username, password
     DBInfo = DB.initDBInfo()
     queries = DB.initQueries()
     return DBInfo, queries
 
 def launch_management_screen(DBInfo):
     # Code to launch the initial GUI screen
+
+args = sys.argv
+
+if len(args) != 0:
+    if len(args) > 1:
+        print("Too manay arguments given.\n")
+    else:
+        db.db_install.createDB()
+else:
+    DBInfo, DBqueries = setup()
+    launch_management_screen(DBInfo)
