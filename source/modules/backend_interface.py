f4df23da30bfbd272dd13052cb1e714e4aa412e2
Nicholasgboland <59149236+Nicholasgboland@users.noreply.github.com>
2024-10-21 15:36:13 -0600

    Create backend_interface.py

---
+++ source/modules/backend_interface.py
@@ -0,0 +1,26 @@
+import sqlite3, json, db
+from sqlite3 import Error
+from string import Formatter
+DB = db.db_interface
+
+
+LIST_PROPERTIES = "LIST_PROPERTIES"
+VIEW_PROPERTY = "VIEW_PROPERTY"
+LIST_UNITS = "LIST_UNITS"
+VIEW_UNIT = "VIEW_UNIT"
+LIST_RENTAL_AGREEMENTS = "LIST_RENTAL_AGREEMENTS"
+VIEW_RENTAL_AGREEMENT = "VIEW_RENTAL_AGREEMENT"
+LIST_RENTAL_INVOICES = "LIST_RENTAL_INVOICES"
+VIEW_RENTAL_INVOICE = "VIEW_RENTAL_INVOICE"
+LIST_EXP_RECORDS = "LIST_EXP_RECORDS"
+VIEW_EXP_RECORDS = "VIEW_EXP_RECORDS"
+LIST_EXP_RECORD_ITEMS = "LIST_EXP_RECORD_ITEMS"
+REPORT_VAR_EXP = "REPORT_VAR_EXP"
+REPORT_FIX_EXP = "REPORT_FIX_EXP"
+
+def list_properties(DBInfo, parms):
+  query = DB.getQuery(LIST_PROPERTIES)
+  for key, value in parms:
+    setParms = DB.setParameters(key, value, parms)
+  sql = DB.buildExecSQL(query, setParms)
+  DB.execSQL(DBInfo, sql)
