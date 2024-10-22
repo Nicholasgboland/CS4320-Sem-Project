20e264b38ca20dce45393219b73ba9b8a30d6978
Nicholasgboland <59149236+Nicholasgboland@users.noreply.github.com>
2024-10-22 08:52:01 -0600

    Update backend_interface.py

--- source/modules/backend_interface.py
+++ source/modules/backend_interface.py
@@ -5,37 +5,37 @@ from string import Formatter
 DB = db.db_interface
 
 
-LIST_PROPERTIES = "LIST_PROPERTIES"
-VIEW_PROPERTY = "VIEW_PROPERTY"
-LIST_UNITS = "LIST_UNITS"
-VIEW_UNIT = "VIEW_UNIT"
-LIST_RENTAL_AGREEMENTS = "LIST_RENTAL_AGREEMENTS"
-VIEW_RENTAL_AGREEMENT = "VIEW_RENTAL_AGREEMENT"
-LIST_RENTAL_INVOICES = "LIST_RENTAL_INVOICES"
-VIEW_RENTAL_INVOICE = "VIEW_RENTAL_INVOICE"
-LIST_EXP_RECORDS = "LIST_EXP_RECORDS"
-VIEW_EXP_RECORDS = "VIEW_EXP_RECORDS"
-LIST_EXP_RECORD_ITEMS = "LIST_EXP_RECORD_ITEMS"
-REPORT_VAR_EXP = "REPORT_VAR_EXP"
-REPORT_FIX_EXP = "REPORT_FIX_EXP"
+LIST_PROPERTIES          = "LIST_PROPERTIES"
+VIEW_PROPERTY            = "VIEW_PROPERTY"
+LIST_UNITS               = "LIST_UNITS"
+VIEW_UNIT                = "VIEW_UNIT"
+LIST_RENTAL_AGREEMENTS   = "LIST_RENTAL_AGREEMENTS"
+VIEW_RENTAL_AGREEMENT    = "VIEW_RENTAL_AGREEMENT"
+LIST_RENTAL_INVOICES     = "LIST_RENTAL_INVOICES"
+VIEW_RENTAL_INVOICE      = "VIEW_RENTAL_INVOICE"
+LIST_EXP_RECORDS         = "LIST_EXP_RECORDS"
+VIEW_EXP_RECORDS         = "VIEW_EXP_RECORDS"
+LIST_EXP_RECORD_ITEMS    = "LIST_EXP_RECORD_ITEMS"
+REPORT_VAR_EXP           = "REPORT_VAR_EXP"
+REPORT_FIX_EXP           = "REPORT_FIX_EXP"
+
+def init_list_properties(DBInfo, queryDict):
+  query = DB.getQuery(LIST_PROPERTIES, queryDict)
+  return query, parms
 
 def list_properties(DBInfo, parms, queryDict):
-  query, parmDict = DB.getQuery(LIST_PROPERTIES, queryDict)
+  query = DB.getQuery(LIST_PROPERTIES, queryDict)
   if parms is not None:
     for key, value in parms:
       setParms = DB.setParameters(key, value, parms)
     sql = DB.buildExecSQL(query, setParms)
   else:
-    setParms = None
     sql = query
   results = DB.execSQL(DBInfo, sql)
   return results
-  if parms is not None:
-    for key, value in parms:
-      setParms = DB.setParameters(key, value, parms)
-    sql = DB.buildExecSQL(query, setParms)
-  else:
-    setParms = None
-    sql = query
-  results = DB.execSQL(DBInfo, sql)
-  return results
+
+def init_view_property(DBInfo, queryDict):
+  query = DB.getQuery(VIEW_PROPERTY, queryDict)
+  parms = DB.initParmDict(query)
+  return query, parms
+  
