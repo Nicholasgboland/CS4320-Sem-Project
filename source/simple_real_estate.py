e37eefdfc68911e212f731f4f180d0f5fa4df74f
Nicholasgboland <nbolandcs@gmail.com>
2024-12-03 19:59:33 -0700

    Final Cleanup

--- source/simple_real_estate.py
+++ source/simple_real_estate.py
@@ -7,7 +7,7 @@ if len(args) != 2:
     print("Incorrect amount of arguments given.\n")
 elif len(args) == 2:
     if args[1] == "-i":
-        os.system('python3 simple_real_estate_1/manage.py makemigrations')
-        os.system('python3 simple_real_estate_1/manage.py migrate')
+        os.system('python simple_real_estate_1/manage.py makemigrations')
+        os.system('python simple_real_estate_1/manage.py migrate')
     elif args[1] == "-r":
-        os.system('python3 simple_real_estate_1/manage.py runserver')
+        os.system('python simple_real_estate_1/manage.py runserver')
